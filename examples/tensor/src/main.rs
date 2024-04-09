aef2069d181956d6908424bdc516b62e54d4ded0
xupremix <xupremix.me@gmail.com>
2024-04-09 02:28:20 +0200

    inner, from_inner autodiff methods

--- examples/tensor/src/main.rs
+++ examples/tensor/src/main.rs
@@ -1,4 +1,4 @@
-// use kindle_burn::backend::Autodiff;
+use kindle_burn::backend::Autodiff;
 use kindle_burn::backend::Wgpu;
 use kindle_burn::define_tensor;
 use kindle_burn::tensor::Shape;
@@ -6,11 +6,11 @@ use kindle_burn::tensor::Shape;
 define_tensor!(vis = pub, dim = 3);
 
 fn main() {
-    // let t = kindle_burn::tensor::Tensor::<Autodiff<Wgpu>, 2>::zeros(
-    //     Shape::new([2; 2]),
-    //     &Default::default(),
-    // );
-    let t = kindle_burn::tensor::Tensor::<Wgpu, 2>::zeros(Shape::new([2; 2]), &Default::default());
+    let t = kindle_burn::tensor::Tensor::<Autodiff<Wgpu>, 2>::zeros(
+        Shape::new([2; 2]),
+        &Default::default(),
+    );
+    // let t = kindle_burn::tensor::Tensor::<Wgpu, 2>::zeros(Shape::new([2; 2]), &Default::default());
     //
     // let t2 = t_cpu + t_gpu;
     // println!("{:?}", t2);
